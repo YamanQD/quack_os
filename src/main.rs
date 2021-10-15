9aca4a80a25dad8a649b0ec0f26ab768bf73bec9
Yaman Qassas <yaman102011@gmail.com>
2021-10-15 21:54:26 +0300

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-#![no_std]
-#![no_main]
+#![no_std] // Disable standard library linking
+#![no_main] // Disable rust's entry point
 use core::panic::PanicInfo;
 
+// Called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+// Entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     loop {}
